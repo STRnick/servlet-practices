83088c6333910ce6788a77247844e3c7e41163d3
STRnick <nicksumin97@naver.com>
2022-05-20 14:29:49 +0900

    servlet/jsp #1

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,27 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 항상 제일 위쪽에 위치해야함. 이유: response.getWriter()에 \n이 들어가는데 
+		// 이걸로 인해 header에 들어갈 정보가 body에 들어가서 못알아먹음.
+		response.setContentType("text/html; charset=utf-8"); 	
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>안녕하세요!! (servelet mapping)</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
