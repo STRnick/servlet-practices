21fabb98e58006fe35a535646256726003a3a7a2
STRnick <nicksumin97@naver.com>
2022-05-24 16:19:57 +0900

    modify

--- guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookServlet.java
+++ guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookServlet.java
@@ -18,7 +18,7 @@ public class GuestbookServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		response.getWriter().println("<h1>OK!</h1>");
+//		response.getWriter().println("<h1>OK!</h1>");
 		
 		String action = request.getParameter("a");
 
